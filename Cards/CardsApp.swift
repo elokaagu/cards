f3ce44af1977fab837537319fffee136b58c84ba
elokaagu <eloka.agu@icloud.com>
2021-07-20 22:56:49 +0100

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Eloka Agu on 20/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
